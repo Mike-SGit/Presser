ea6b0323fcdf8f27653cea0647a4b3343e97088c
Mike-SGit <mike.sam2322@gmail.com>
2021-09-25 18:00:14 +1000

    client server and key listening

---
+++ keypressee/src/App.java
@@ -0,0 +1,36 @@
+import java.io.*;  
+import java.net.*;  
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Listener keylis = new Listener();
+
+        try {
+			GlobalScreen.registerNativeHook();
+		}
+		catch (NativeHookException ex) {
+			System.err.println("There was a problem registering the native hook.");
+			System.err.println(ex.getMessage());
+
+			System.exit(1);
+		}
+        GlobalScreen.addNativeKeyListener(keylis);
+       
+
+        try{      
+
+            Socket s=new Socket("localhost",6666);  
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+            dout.writeUTF("Hello Server");  
+            dout.flush();  
+            dout.close();  
+            s.close();  
+            }catch(Exception e){System.out.println(e);}  
+            } 
+    }
---
+++ keypressee/src/Listener.java
@@ -0,0 +1,35 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+public class Listener implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException nativeHookException) {
+                nativeHookException.printStackTrace();
+            }
+        }
+    }
+    
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        String pressed = NativeKeyEvent.getKeyText(e.getKeyCode());
+        // System.out.println("\n===========\n You pressed " + pressed + " \n\n ==========\n");
+        char presc = pressed.charAt(0);
+        // System.out.println("\n===========\n As char.. " + presc + " \n\n ==========\n");
+        if (pressed.contains("C")){
+            System.out.println("\n===========\n As char.. I TWAS SEEEE"  + " \n\n ==========\n");
+        }
+        System.out.println("\n===========\n As char.. " + presc + " \n\n ==========\n");
+
+    }
+    
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+}
